05b87c71e06b6c695f3ac20eec5b0fb7f52890b1
andreadlm <delmastro2000@gmail.com>
2024-03-21 11:46:57 +0100

    inizializzazione e configurazione per fileService

---
+++ fileService/src/main/java/com/appunto/fileservice/FileServiceApplication.java
@@ -0,0 +1,15 @@
+package com.appunto.fileservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FileServiceApplication.class, args);
+
+    System.out.println("Hello world!");
+  }
+
+}
