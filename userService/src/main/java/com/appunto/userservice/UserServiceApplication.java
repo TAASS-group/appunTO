4d6d8df79aeb05274f9cbc17bfd644fca2552c3e
andreadlm <delmastro2000@gmail.com>
2024-03-22 14:25:53 +0100

    Inizializzazione di Firebase

--- userService/src/main/java/com/appunto/userservice/UserServiceApplication.java
+++ userService/src/main/java/com/appunto/userservice/UserServiceApplication.java
@@ -1,15 +1,32 @@
 package com.appunto.userservice;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import lombok.extern.java.Log;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication @Log
 public class UserServiceApplication {
-
   public static void main(String[] args) {
     SpringApplication.run(UserServiceApplication.class, args);
+  }
 
-    System.out.println("Hello world");
+  @Bean
+  public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+    return args -> {
+      FirebaseOptions options = FirebaseOptions.builder()
+          .setCredentials(GoogleCredentials.getApplicationDefault())
+          .build();
+
+      FirebaseApp.initializeApp(options);
+
+      log.info("Firebase initialized correctly");
+    };
   }
 
 }
