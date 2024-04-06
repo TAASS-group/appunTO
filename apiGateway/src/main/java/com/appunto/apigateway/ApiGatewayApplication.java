0749fa96a503e8320a4422a58cfa65b329d733ac
andreadlm <delmastro2000@gmail.com>
2024-04-06 16:36:22 +0200

    Pulizia import

--- apiGateway/src/main/java/com/appunto/apigateway/ApiGatewayApplication.java
+++ apiGateway/src/main/java/com/appunto/apigateway/ApiGatewayApplication.java
@@ -2,9 +2,6 @@ package com.appunto.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
