7743822a3e74d4e3636eaa4ec285457c574dea66
andreadlm <delmastro2000@gmail.com>
2024-04-07 14:57:05 +0200

    Eureka server funzionante

--- userService/src/main/java/com/appunto/userservice/UserServiceApplication.java
+++ userService/src/main/java/com/appunto/userservice/UserServiceApplication.java
@@ -7,10 +7,12 @@ import lombok.extern.java.Log;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication @Log
+@EnableDiscoveryClient
 public class UserServiceApplication {
   public static void main(String[] args) {
     SpringApplication.run(UserServiceApplication.class, args);
