4905abb9504ffa37d41059d5be300e8462a3c602
kuraikara@outlook.it <kuraikara@outlook.it>
2024-05-02 15:19:33 +0200

    home design

--- frontend/app/(authpages)/login/page.tsx
+++ frontend/app/(authpages)/login/page.tsx
@@ -1,5 +1,62 @@
-import React from "react";
+import { Metadata } from "next";
+import Image from "next/image";
+import Link from "next/link";
 
-export default function LoginPage() {
-  return <div>LoginPage</div>;
+import { cn } from "@/lib/utils";
+import { buttonVariants } from "@/components/ui/button";
+import { UserAuthForm } from "@/components/auth/UserAuthForm";
+
+export const metadata: Metadata = {
+  title: "Authentication",
+  description: "Authentication forms built using the components.",
+};
+
+export default function AuthenticationPage() {
+  return (
+    <>
+      <div className="container relative  h-screen flex-col items-center justify-center grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+        <Link
+          href="/examples/authentication"
+          className={cn(
+            buttonVariants({ variant: "ghost" }),
+            "absolute right-4 top-4 md:right-8 md:top-8"
+          )}
+        >
+          Register
+        </Link>
+        <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
+          <div className="absolute inset-0 bg-zinc-900" />
+          <div className="relative z-20 flex items-center text-lg font-medium">
+            <Image
+              src="/logowhite.png"
+              width={150}
+              height={150}
+              alt="logo"
+              objectFit="contain"
+              objectPosition="left"
+              className=""
+            />
+          </div>
+          <div className="relative z-20 mt-auto">
+            <blockquote className="space-y-2">
+              <p className="text-lg">Qualocsa</p>
+            </blockquote>
+          </div>
+        </div>
+        <div className="lg:p-8">
+          <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+            <div className="flex flex-col space-y-2 text-center">
+              <h1 className="text-2xl font-semibold tracking-tight">
+                Login in your account
+              </h1>
+              <p className="text-sm text-muted-foreground">
+                Enter your email and password below to access your account
+              </p>
+            </div>
+            <UserAuthForm />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
