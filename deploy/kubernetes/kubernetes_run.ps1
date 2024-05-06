57eda408e88129fafc2e0e4c65b7eef8dc50c5f5
Giulia Frumento <giuliafrumento23@gmail.com>
2024-05-06 12:45:36 +0200

    Script windows kubernetes

---
+++ deploy/kubernetes/kubernetes_run.ps1
@@ -0,0 +1,11 @@
+# Per genereare i file di configurazione tramite kompose:
+# kompose convert -f compose.yaml -o config/
+
+docker compose -p appunto -f compose.yaml build
+kubectl apply -f ./config
+
+# Per eseguire il port forward
+# Start-Sleep -seconds 10
+# kubectl port-forward service/userservice-db 5433:5432 > /dev/null &
+# kubectl port-forward service/eurekaserver-app 8761:8761 > /dev/null &
+# ...
