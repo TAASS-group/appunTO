21c19f8ff6cd7115a87bfcfd19e1f22584649cdf
monex00 <mone.00@hotmail.it>
2024-05-12 13:44:21 +0200

    feat: added notification at frontend

---
+++ frontend/components/home/NotificationButton.tsx
@@ -0,0 +1,123 @@
+"use client";
+
+import * as React from "react";
+import { DropdownMenuCheckboxItemProps } from "@radix-ui/react-dropdown-menu";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { ScrollArea } from "../ui/scroll-area";
+import { NotificationContext } from "@/providers/NotificationProvider";
+import { Separator } from "../ui/separator";
+import { Bell, BellIcon } from "lucide-react";
+import { Badge } from "../ui/badge";
+
+export function NotificationButton() {
+  let { notifications, setNotifications } =
+    React.useContext(NotificationContext);
+  React.useEffect(() => {
+    setNotifications([
+      {
+        id: 1,
+        message: "The course has been updated with new content and assignments",
+        title: "Test Title",
+        course: "Test Course",
+        timestamp: "2022-01-01T00:00:00Z",
+      },
+      {
+        id: 1,
+        message: "The course has been updated with new content and assignments",
+        title: "Test Title",
+        course: "Test Course",
+        timestamp: "2022-01-01T00:00:00Z",
+      },
+      {
+        id: 1,
+        message: "The course has been updated with new content and assignments",
+        title: "Test Title",
+        course: "Test Course",
+        timestamp: "2022-01-01T00:00:00Z",
+      },
+      {
+        id: 1,
+        message: "The course has been updated with new content and assignments",
+        title: "Test Title",
+        course: "Test Course",
+        timestamp: "2022-01-01T00:00:00Z",
+      },
+      {
+        id: 1,
+        message: "The course has been updated with new content and assignments",
+        title: "Test Title",
+        course: "Test Course",
+        timestamp: "2022-01-01T00:00:00Z",
+      },
+      {
+        id: 1,
+        message: "The course has been updated with new content and assignments",
+        title: "Test Title",
+        course: "Test Course",
+        timestamp: "2022-01-01T00:00:00Z",
+      },
+      {
+        id: 1,
+        message: "The course has been updated with new content and assignments",
+        title: "Test Title",
+        course: "Test Course",
+        timestamp: "2022-01-01T00:00:00Z",
+      },
+      {
+        id: 1,
+        message: "The course has been updated with new content and assignments",
+        title: "Test Title",
+        course: "Test Course",
+        timestamp: "2022-01-01T00:00:00Z",
+      },
+    ]);
+  }, []);
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button className="relative" variant="ghost" size="icon">
+          <Bell className="h-[1.5rem] w-[1.5rem]"></Bell>
+          {notifications.length > 0 && (
+            <span className="h-[10px] w-[10px] rounded-full bg-primary absolute top-2 right-[8px] border-[1px] border-background"></span>
+          )}
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-[500px] max-h-72">
+        <ScrollArea className="w-full h-72  rounded-md">
+          <div className="p-4">
+            <div className="mb-4 flex justify-between items-center">
+              <h4 className="text-xl font-bold leading-none">Notifications</h4>
+              <Badge className="rounded-full">{notifications.length}</Badge>
+            </div>
+            <div className="flex flex-col-reverse">
+              {notifications.map((notification: any, index: number) => (
+                <div key={index}>
+                  <div className="-mx-2 flex items-start space-x-4 rounded-md p-2 transition-all hover:bg-accent hover:text-accent-foreground">
+                    <div className="space-y-1">
+                      <p className="text-sm font-medium leading-none">
+                        {notification.title}
+                      </p>
+                      <p className="text-sm text-muted-foreground">
+                        {notification.message}
+                      </p>
+                      <p className="text-xs text-muted-foreground text-end">
+                        {new Date(notification.timestamp).toLocaleString()}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </ScrollArea>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
---
+++ frontend/providers/NotificationProvider.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import * as React from "react";
+
+export const NotificationContext = React.createContext({} as any);
+
+export function NotificationProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [notifications, setNotifications] = React.useState<any[]>([]);
+  const [ws, setWs] = React.useState<WebSocket | null>(null);
+
+  React.useEffect(() => {
+    const socket = new WebSocket("ws://localhost:8085/ws");
+    socket.onopen = () => {
+      // userid:courseId
+      socket.send("1:test1");
+      console.log("Socket connected");
+    };
+    socket.onmessage = (event) => {
+      console.log(event.data);
+      const message = JSON.parse(event.data);
+      setNotifications((prev: any[]) => [...prev, message]);
+    };
+    socket.onerror = function (error) {
+      console.error("WebSocket Error: ", error);
+    };
+
+    socket.onclose = (event) => {
+      console.log("Socket closed", event.code, event.reason);
+      setWs(null);
+    };
+
+    setWs(socket);
+
+    return () => {
+      socket.close();
+    };
+  }, []);
+
+  return (
+    <NotificationContext.Provider value={{ notifications, setNotifications }}>
+      {children}
+    </NotificationContext.Provider>
+  );
+}
