af3f854560b2d78e588cd67897022465ca85e722
Kilyez <mattiamondino2000@gmail.com>
2024-05-14 18:03:55 +0200

    graphic done

--- frontend/app/(navigablepages)/[course_id]/forum/answer/page.tsx
+++ frontend/app/(navigablepages)/[course_id]/forum/answer/page.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+"use client";
+import React, { useState } from "react";
 
-const page = () => {
+import Answers from "@/components/forum/Answers";
+import QuestionInAnswer from "@/components/forum/QuestionInAnswer";
+
+import { Separator } from "@/components/ui/separator";
+
+import { ComboboxDemo } from "@/components/forum/combobox";
+import { answers } from "@/components/forum/data/answers";
+
+const Page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <div className=" border bg-card text-card-foreground shadow my-8  space-y-4">
+      <QuestionInAnswer />
+      <Separator />
 
-export default page
+      <div className="flex justify-between pl-6 pr-6">
+        <h3 className="text-xl font-semibold leading-none tracking-tight text-center mx-4 my-4">
+          {" "}
+          Answers
+        </h3>
+        <ComboboxDemo />
+      </div>
+      {answers.map((answer) => (
+        <Answers key={answer.id} answer={answer} />
+      ))}
+    </div>
+  );
+};
+
+export default Page;
---
+++ frontend/components/forum/data/answers.ts
@@ -0,0 +1,50 @@
+export interface AnswerType {
+    text: string;
+    id: number;
+    userid: number;
+    upvotes: number;
+}
+
+export const answers: AnswerType[] = [
+    {
+        text: "I think this is the answer",
+        id: 1,
+        userid: 1,
+        upvotes: 5,
+    },
+    {
+        text: "I think this is the answer",
+        id: 2,
+        userid: 2,
+        upvotes: 3,
+    },
+    {
+        text: "I think this is the answer",
+        id: 3,
+        userid: 3,
+        upvotes: 2,
+    },
+    {
+        text: "I think this is the answer",
+        id: 4,
+        userid: 4,
+        upvotes: 1,
+    },
+    {
+        text: "I think this is the answer",
+        id: 5,
+        userid: 5,
+        upvotes: 0,
+    },
+    {
+        text: "I think this is the answer",
+        id: 6,
+        userid: 6,
+        upvotes: 0,
+    },
+    {
+        text: "I think this is the answer",
+        id: 7,
+        userid: 7,
+        upvotes: 0,
+    },];
